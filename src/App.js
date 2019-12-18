7505d31fff226d5a9a79873f9c61285b8eaebc6b
soulcactus <sunhyejin90@naver.com>
2019-12-18 16:39:29 +0900

    💎 Feat : Add splash

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from "react-native";
 import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from "react-native/Libraries/NewAppScreen";
 import Login from "./pages/Login.js";
 
+import SplashScreen from "react-native-splash-screen";
+
 const App = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
     return (
         <>
-            <StatusBar barStyle="dark-content" />
+            <StatusBar barStyle="dark-content" backgroundColor={"transparent"} />
             <SafeAreaView>
                 <Login />
             </SafeAreaView>
