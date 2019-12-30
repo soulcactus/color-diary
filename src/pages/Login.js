142963970ff0646d0b477294ccd533e6913e53ed
soulcactus <sunhyejin90@naver.com>
2019-12-30 12:57:40 +0900

    🔧 Fix : Add StylesSheet

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from "react-native";
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button } from "react-native";
 import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from "react-native/Libraries/NewAppScreen";
 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: `red`
+    },
+    naver: {
+        backgroundColor: `green`
+    }
+});
+
 const Login = () => {
-    return <Text>test</Text>;
+    return (
+        <View style={styles.container}>
+            <Button style={styles.naver}>네이버 로그인</Button>
+        </View>
+    );
 };
 
 export default Login;
