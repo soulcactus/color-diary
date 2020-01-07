e45417bc98df1200d851a440e313274d53930a47
soulcactus <sunhyejin90@naver.com>
2020-01-07 12:46:57 +0900

    💎 Feat : Add kakao settings

--- android/app/src/main/java/com/colordiary/MainApplication.java
+++ android/app/src/main/java/com/colordiary/MainApplication.java
@@ -4,10 +4,12 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.dooboolab.kakaologins.RNKakaoLoginsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.dooboolab.kakaologins.RNKakaoLoginsPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new RNKakaoLoginsPackage());
           return packages;
         }
 
