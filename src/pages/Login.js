cb66e3fc621aa0359eb521c23ee6cf4ff8d8eef5
soulcactus <sunhyejin90@naver.com>
2020-01-07 12:47:12 +0900

    🔧 Fix : Fix login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button } from "react-native";
 import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from "react-native/Libraries/NewAppScreen";
+import RNKakaoLogins from "react-native-kakao-logins";
 
 const styles = StyleSheet.create({
     container: {
@@ -14,7 +15,21 @@ const styles = StyleSheet.create({
 const Login = () => {
     return (
         <View style={styles.container}>
-            <Button style={styles.naver}>네이버 로그인</Button>
+            <Button style={styles.kakao} title="카카오톡 로그인">
+                카카오톡 로그인
+            </Button>
+            <Button style={styles.naver} title="네이버 로그인">
+                네이버 로그인
+            </Button>
+            <Button style={styles.facebook} title="페이스북 로그인">
+                페이스북 로그인
+            </Button>
+            <Button style={styles.google} title="구글 로그인">
+                구글 로그인
+            </Button>
+            <Button style={styles.email} title="이메일 로그인">
+                이메일 로그인
+            </Button>
         </View>
     );
 };
